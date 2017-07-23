643a74d316e7bc5d1b64a1d98f8e410ae72e4991
nvanandsah <navneet16170@iiitd.ac.in>
2017-07-23 10:48:12 -0700

    Final code

---
+++ classfinal/Assets/SimpleCharacterControl1.cs
@@ -0,0 +1,264 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+public class SimpleCharacterControl1 : NetworkBehaviour {
+	private readonly string[] m_animations = { "Wave" };
+	private Animator[] m_animators;
+	[SerializeField] private CameraLogic m_cameraLogic;
+
+    private enum ControlMode
+    {
+        Tank,
+        Direct
+    }
+
+    [SerializeField] private float m_moveSpeed = 2;
+    [SerializeField] private float m_turnSpeed = 200;
+    [SerializeField] private float m_jumpForce = 4;
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private Rigidbody m_rigidBody;
+
+    [SerializeField] private ControlMode m_controlMode = ControlMode.Direct;
+
+    private float m_currentV = 0;
+    private float m_currentH = 0;
+
+    private readonly float m_interpolation = 10;
+    private readonly float m_walkScale = 0.33f;
+    private readonly float m_backwardsWalkScale = 0.16f;
+    private readonly float m_backwardRunScale = 0.66f;
+
+    private bool m_wasGrounded;
+    private Vector3 m_currentDirection = Vector3.zero;
+
+    private float m_jumpTimeStamp = 0;
+    private float m_minJumpInterval = 0.25f;
+
+    private bool m_isGrounded;
+    private List<Collider> m_collisions = new List<Collider>();
+
+	private void Start()
+	{
+		m_animators = FindObjectsOfType<Animator>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        for(int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                if (!m_collisions.Contains(collision.collider)) {
+                    m_collisions.Add(collision.collider);
+                }
+                m_isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        bool validSurfaceNormal = false;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                validSurfaceNormal = true; break;
+            }
+        }
+
+        if(validSurfaceNormal)
+        {
+            m_isGrounded = true;
+            if (!m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Add(collision.collider);
+            }
+        } else
+        {
+            if (m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Remove(collision.collider);
+            }
+            if (m_collisions.Count == 0) { m_isGrounded = false; }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(m_collisions.Contains(collision.collider))
+        {
+            m_collisions.Remove(collision.collider);
+        }
+        if (m_collisions.Count == 0) { m_isGrounded = false; }
+    }
+
+	void Update () {
+
+		if (!isLocalPlayer) {
+			return;
+		}
+        m_animator.SetBool("Grounded", m_isGrounded);
+
+        switch(m_controlMode)
+        {
+            case ControlMode.Direct:
+                DirectUpdate();
+                break;
+
+            case ControlMode.Tank:
+                TankUpdate();
+                break;
+
+            default:
+                Debug.LogError("Unsupported state");
+                break;
+        }
+
+        m_wasGrounded = m_isGrounded;
+    }
+
+    private void TankUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        bool walk = Input.GetKey(KeyCode.LeftShift);
+
+        if (v < 0) {
+            if (walk) { v *= m_backwardsWalkScale; }
+            else { v *= m_backwardRunScale; }
+        } else if(walk)
+        {
+            v *= m_walkScale;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
+        transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
+
+        m_animator.SetFloat("MoveSpeed", m_currentV);
+
+        JumpingAndLanding();
+    }
+
+    private void DirectUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        Transform camera = Camera.main.transform;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            v *= m_walkScale;
+            h *= m_walkScale;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+
+        float directionLength = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized * directionLength;
+
+        if(direction != Vector3.zero)
+        {
+            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+
+            transform.rotation = Quaternion.LookRotation(m_currentDirection);
+            transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
+
+            m_animator.SetFloat("MoveSpeed", direction.magnitude);
+        }
+
+        JumpingAndLanding();
+    }
+
+    private void JumpingAndLanding()
+    {
+        bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
+
+        if (jumpCooldownOver && m_isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            m_jumpTimeStamp = Time.time;
+            m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+        }
+
+        if (!m_wasGrounded && m_isGrounded)
+        {
+            m_animator.SetTrigger("Land");
+        }
+
+        if (!m_isGrounded && m_wasGrounded)
+        {
+            m_animator.SetTrigger("Jump");
+        }
+    }
+	private void OnGUI()
+	{
+		if (!isLocalPlayer) {
+			return;
+		}
+		GUILayout.BeginVertical(GUILayout.Width(Screen.width));
+
+		GUILayout.BeginHorizontal();
+
+		/*       if (GUILayout.Button("Previous character (Q)"))
+        {
+            m_cameraLogic.PreviousTarget();
+        }
+
+        if (GUILayout.Button("Next character (E)"))
+        {
+            m_cameraLogic.NextTarget();
+        }
+*/
+		GUILayout.EndHorizontal();
+
+		GUILayout.Space(16);
+
+		for(int i = 0; i < m_animations.Length; i++)
+		{
+			if(i == 0) { GUILayout.BeginHorizontal(); }
+
+			if(GUILayout.Button(m_animations[i]))
+			{
+				for(int j = 0; j < m_animators.Length; j++)
+				{
+					m_animators[j].SetTrigger(m_animations[i]);
+				}
+			}
+
+			if(i == m_animations.Length - 1) { GUILayout.EndHorizontal(); }
+			else if (i == (m_animations.Length / 2)) { GUILayout.EndHorizontal(); GUILayout.BeginHorizontal(); }
+		}
+
+		GUILayout.Space(16);
+
+		Color oldColor = GUI.color;
+		GUI.color = Color.black;
+				/*
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("Left Shift: Walk");
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("Space: Jump");
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+*/
+		GUI.color = oldColor;
+
+		GUILayout.EndVertical();
+	}
+}
---
+++ classfinal/Assets/WebCamDetect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Collections;
+public class WebCamDetect : MonoBehaviour
+{	public string Ishost = "You are the teacher";
+	void OnGUI(){
+		GUI.Label (new Rect (10, 10, 10, 10), Ishost);
+	}
+	public bool isSet = true;
+	void Start()
+	{	
+		WebCamDevice[] devices = WebCamTexture.devices;
+		Debug.Log ("Number of web cams connected: " + devices.Length);
+
+		for (int i = 0; i < devices.Length; i++) {
+			Debug.Log (i + " " + devices [i].name);
+		}
+
+		Renderer rend = this.GetComponentInChildren<Renderer> ();
+
+		WebCamTexture mycam = new WebCamTexture ();
+		string camName = devices [0].name;
+		Debug.Log ("The webcam name is " + camName);
+		mycam.deviceName = camName;
+		rend.material.mainTexture = mycam;
+
+		mycam.Play ();
+		isSet = false;
+	}
+	void Update(){
+		if (!Network.isServer) {
+			Debug.Log ("Student entered the class");
+			Ishost = "You are Studnet";
+		}
+	}
+}
